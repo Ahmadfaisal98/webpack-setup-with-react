67c1c54d355b3f101023712a924ae63481cae4fe
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-02-16 22:13:53 +0700

    finish setup-webpack 5 react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,5 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes";
-import image from "../images/29093880_180400312758202_3062336683052630016_n.jpg";
-import imageSvg from "../images/check-svg.svg";
 import Whatever from "./Whatever";
 
 const App = () => {
@@ -12,8 +10,6 @@ const App = () => {
         <section>
           <h1>Oh Hai, React</h1>
         </section>
-        <img src={image} alt="people" width={250} />
-        <img src={imageSvg} alt="people" width={250} />
         <Recipes />
         <Whatever />
       </main>
